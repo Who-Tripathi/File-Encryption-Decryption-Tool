2a720d1b7448fa7781caf817e36555e2906ddbbd
who-tripathi <whotripathi@gmail.com>
2025-08-14 20:07:31 +0530

    Encryption class added and configured

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import encryption.FileEncryptionAndDecryption;
+
+import java.io.*;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("---Welcome to the File encryption and Decryption Program---");
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Type 'E' for encryption and 'D' for decryption : ");
+        String choice = sc.nextLine();
+
+        System.out.print("Please enter the complete file path with the filename which you wants to encrypt : ");
+        String fileName = sc.nextLine();
+        System.out.print("Please enter your encryption key here(make sure you are passing integer) : ");
+        Integer encryptionKey = sc.nextInt();
+    try{
+        if (choice.equals("E")) {
+            FileEncryptionAndDecryption.fileEncryptionAndDecryption(fileName, encryptionKey,choice);
+            System.out.println("File encryption has been successfully completed");
+        } else if (choice.equals("D")) {
+            FileEncryptionAndDecryption.fileEncryptionAndDecryption(fileName, encryptionKey, choice);
+            System.out.println("File decryption has been successfully completed");
+        }
+    }catch (Exception e){
+        System.out.println("File encryption has been failed...file not found");
+    }
     }
 }
--- src/encryption/FileEncryptionAndDecryption.java
+++ src/encryption/FileEncryptionAndDecryption.java
@@ -1,8 +1,23 @@
 package encryption;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class FileEncryptionAndDecryption {
 
-    public static void fileEncryptionAndDecryption(String filePath, Integer encryptionKey) {
+    public static void fileEncryptionAndDecryption(String filePath, Integer encryptionKey, String choice)throws Exception {
+        File file = new File(filePath);
+        String relativePath=file.getParent();
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filePath));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(relativePath+(choice.equals("E")?"\\encryptedFile.txt":"\\decryptedFile.txt"))));
 
+        int data;
+        while ((data=bis.read())!=-1){
+            bos.write(data^encryptionKey);
+        }
+        file.deleteOnExit();
+        bos.close();
+        bis.close();
     }
 }
